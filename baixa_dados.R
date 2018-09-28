91cd3de02fea05b8372f96312caf359e17c50aa5
belisards <adrianobf@gmail.com>
2018-09-28 16:44:54 -0300

    Update baixa_dados.R

--- baixa_dados.R
+++ baixa_dados.R
@@ -1,7 +1,7 @@
 #Baixa dados de servidores do Ceará
 #Truco - Agência Pública - setembro 2018
 baseano <- 2015:2018
-meses <- c("08","09","10","11","12")
+meses <- c("01","02","03","04","05","06","07","08","09","10","11","12")
 #Loop para baixar os arquivos do TSE - trocar por lapply ou simliar
 for(ano in baseano){
   for(mes in meses) {
